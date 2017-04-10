b9455237953b0149664fe0a8fe77ac1349afa3db
Cem Dırman <cemdrman@gmail.com>
2017-04-11 00:48:41 +0300

    project created

--- app/src/main/java/com/example/cem/combilisimiovolley/MainActivity.java
+++ app/src/main/java/com/example/cem/combilisimiovolley/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cem.combilisimiovolley;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -9,37 +10,30 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String requestLink = "https://api.apixu.com/v1/current.json?key=039e4db4333a482dabf214535170904&q=Istanbul";
+    private TextView textView1;
+    private TextView textView2;
+    private TextView textView3;
+    private TextView textView4;
+    private ImageView icon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView textView1 = (TextView) findViewById(R.id.txt1);
-        final TextView textView2 = (TextView) findViewById(R.id.txt2);
-        final TextView textView3 = (TextView) findViewById(R.id.txt3);
-        final TextView textView4 = (TextView) findViewById(R.id.txt4);
+        defineComponent();
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         StringRequest stringRequest = new StringRequest(Request.Method.GET, requestLink, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                try {
-                    JSONObject jsonObject = new JSONObject(response);
-                    JSONObject jsonCurrent = jsonObject.getJSONObject("current");
-                    JSONObject jsonLocation = jsonObject.getJSONObject("location");
-                    JSONObject jsonCondition = jsonCurrent.getJSONObject("condition");
-                    textView1.setText(jsonLocation.getString("region") + "-" +jsonLocation.getString("country"));
-                    textView2.setText("Durum: " + jsonCondition.getString("text"));
-                    textView3.setText("sıcaklık: " + jsonCurrent.getString("temp_c"));
-                    textView4.setText("hissedilen sıcaklık: " + jsonCurrent.getString("feelslike_c"));
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                parseResponse(response);
             }
         }, new Response.ErrorListener() {
             @Override
@@ -48,5 +42,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         requestQueue.add(stringRequest);
+    }
+
+    private void defineComponent(){
+        textView1 = (TextView) findViewById(R.id.txt1);
+        textView2 = (TextView) findViewById(R.id.txt2);
+        textView3 = (TextView) findViewById(R.id.txt3);
+        textView4 = (TextView) findViewById(R.id.txt4);
+        icon = (ImageView) findViewById(R.id.icon);
+    }
+
+    private void parseResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONObject jsonCurrent = jsonObject.getJSONObject("current");
+            JSONObject jsonLocation = jsonObject.getJSONObject("location");
+            JSONObject jsonCondition = jsonCurrent.getJSONObject("condition");
+            textView1.setText(jsonLocation.getString("region") + "-" +jsonLocation.getString("country"));
+            textView2.setText("Durum: " + jsonCondition.getString("text"));
+            textView3.setText("sıcaklık: " + jsonCurrent.getString("temp_c"));
+            textView4.setText("hissedilen sıcaklık: " + jsonCurrent.getString("feelslike_c"));
+            //Picasso.with(this).load()
+            Picasso.with(this).load(jsonCondition.getString("icon")).into(icon);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
